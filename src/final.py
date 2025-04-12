450795c73a9c97acaa06127b84771de8e0c722b0
Henry Morrison <alexmrsn1@gmail.com>
2025-04-12 16:26:22 -0500

    Made it so the image is saved to the project file

--- src/final.py
+++ src/final.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from PIL import Image
 
+    
 url = input("Paste the URL of the image you wish to color pick from: ")
-img = Image.open(urlopen(url))
-img.show()
+img = urlopen(url)
+with open ("userimg.png", "wb") as file:
+    file.write(img.read())
