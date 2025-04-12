2a8882734e4bd4052104b664c387a87464b7ab12
Henry Morrison <alexmrsn1@gmail.com>
2025-04-12 16:40:14 -0500

    Trying to make the image paste in UI rather than the terminal

--- src/final.py
+++ src/final.py
@@ -1,8 +1,19 @@
 from urllib.request import urlopen
-from PIL import Image
+import PySimpleGUI as sg
+from PIL import Image, ImageTk
+import io
 
-    
-url = input("Paste the URL of the image you wish to color pick from: ")
-img = urlopen(url)
-with open ("userimg.png", "wb") as file:
-    file.write(img.read())
+def download_img(url):
+    img = urlopen(url)
+    with open ("userimg.png", "wb") as file:
+        file.write(img.read())
+
+
+layout = [
+    [sg.Text("Paste the URL of the image you wish to color pick from: ")],
+    [sg.InputText(key = "url_input")],
+    [sg.Image(filename = "user_img", key = "image")],
+    [sg.Button("Done")]
+]
+
+window = sg.Window("Color Picker", layout)
